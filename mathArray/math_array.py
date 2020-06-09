c78872aa946852811a76fdba7c2cea5410aef038
Brock Ryan <brock.ryan2@gmail.com>
2020-06-09 00:22:46 -0400

    add elapsed time feature

--- mathArray/math_array.py
+++ mathArray/math_array.py
@@ -1,9 +1,27 @@
 #math_array
 from random import randint
+from datetime import datetime
 import time, sys
 
 global count
 count = 0
+
+global start_time
+start_time = datetime.now()
+
+
+def time_function(time_in_seconds):
+  if 3600 > time_in_seconds > 60:
+    minutes = time_in_seconds // 60
+    seconds = time_in_seconds % 60
+
+    return f'{minutes:02}:{seconds:02}'
+
+  elif time_in_seconds < 60:
+    return f'0:{time_in_seconds:02}'
+
+  elif 86400 > time_in_seconds > 3600:
+    return " entirely too long!  You should consider getting a life.\n \nSeriously"
 
 def createArray():
   global count
@@ -12,7 +30,7 @@ def createArray():
   total = rows * columns
   character_fill = " o "
 
-  print("Array #{}:\n".format(str(count+1)))
+  print("Array #{}:\n".format(str(count + 1)))
 
   for i in range(0, rows):
     print(character_fill * columns, end='\n\n')
@@ -30,8 +48,11 @@ def get_user_input(prompt):
   elif str(user_input).lower() == "quit" or str(user_input).lower() == "exit" or str(user_input).lower() == "q" or str(user_input).lower() == "e":
     return "exit"
 
+  elif user_input == None or user_input == "" or user_input == " ":
+    return ""
+
   else:
-    print("Please only enter whole numbers!")
+    print("Please only enter whole numbers!\n")
     return None
 
 def evaluate_user_input(user_input, expected_value, rows, columns):
@@ -43,16 +64,27 @@ def evaluate_user_input(user_input, expected_value, rows, columns):
 
   elif user_input != expected_value and (user_input == rows or user_input == columns) and (expected_value != rows*columns):
     print("\nOops!!  It looks like you may have confused rows with columns.")
-    print("Remember that rows are groups of items that stretch from left to right")
-    print("and columns are groups of items that stretch up and down.\n\nTry again!\n")
+    print("Remember that rows are groups of items that run from left to right")
+    print("and columns are groups of items that run up and down.\n\nTry again!\n")
 
   elif user_input == 'exit':
+    end_time = datetime.now()
+    elapsed_time = end_time - start_time
+
+    normalized_time = time_function(int(elapsed_time.total_seconds()))
+
     if count > 2:
       print("\n\nWay to go!  You got {} right!!  Come back later for more practice.".format(count))
     else:
       print("\n\nYou got {} right. Come back later for more practice.".format(count))
-    time.sleep(6)
+
+    print("Total time = {} ".format(normalized_time))
+
+    time.sleep(5)
     sys.exit(0)
+
+  elif user_input == None or user_input == "":
+    return None
 
   else:
     print("Nope, that's not right. Try again!\n")
@@ -93,6 +125,8 @@ def main():
     check_rows(rows, columns)
     check_columns(rows, columns)
     check_total(rows, columns)
+
+    evaluate_user_input(get_user_input("Press enter to continue. To quit, type 'quit' then press enter.\n"), None, None, None)
 
 if __name__ == '__main__':
   main()
